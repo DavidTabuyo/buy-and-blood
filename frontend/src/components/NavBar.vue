d1a9ece54cf32c7d45494937d0924f7619ac861b
dario <dariommsr71@gmail.com>
2025-03-19 21:54:56 +0100

    FEAT: Navbar set and tailwind set

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="fixed w-full top-0 z-50 shadow-md">
+        <div>
+    <Menubar :model="items" class="px-4 lg:px-8">
+        <template #start>
+            <h1 class="text-lg font-bold">Mi Sitio</h1>
+        </template>
+    </Menubar>
+</div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Menubar from 'primevue/menubar';
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Features',
+        icon: 'pi pi-star'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+            {
+                label: 'Components',
+                icon: 'pi pi-bolt'
+            },
+            {
+                label: 'Blocks',
+                icon: 'pi pi-server'
+            },
+            {
+                label: 'UI Kit',
+                icon: 'pi pi-pencil'
+            },
+            {
+                label: 'Templates',
+                icon: 'pi pi-palette',
+                items: [
+                    {
+                        label: 'Apollo',
+                        icon: 'pi pi-palette'
+                    },
+                    {
+                        label: 'Ultima',
+                        icon: 'pi pi-palette'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+    }
+]);
+</script>
