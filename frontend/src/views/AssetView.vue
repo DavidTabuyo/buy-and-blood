15016f01b3fd0ab39679361b0a94b7dcfea5ac8c
dario <dariommsr71@gmail.com>
2025-03-26 22:07:27 +0100

    FEAT: Asset page structure

---
+++ frontend/src/views/AssetView.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex w-full">
+        <!-- Datos del activo -->
+        <div class="w-2/3">
+            <div>Fidelity S&P 500 Index Fund EUR P Acc</div>
+            <div class="flex">
+                <div>234,23$</div>
+                <div>^0,23%</div>
+            </div>
+            <div>
+                <iframe :src="`https://s.tradingview.com/widgetembed/?symbol=MIL-VUAA&interval=D&theme=light`"
+                    width="100%" height="500" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <!-- Compra venta -->
+        <div class="w-1/3">
+            <div>Mi Total:</div>
+            <div>$12.234,34</div>
+            <div class="flex">
+                <div>+$4233.23</div>
+                <div>^23,23%</div>
+            </div>
+            <div class="flex">
+                <Button>Comprar</Button>
+                <Button>Vender</Button>
+            </div>
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber v-model="value" inputId="on_label" mode="currency" currency="USD" locale="en-US" />
+                    <label for="on-label">Introduce cantidad...</label>
+                </FloatLabel>
+            </div>
+            <Button>Confirmar</Button>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import InputNumber from 'primevue/inputnumber';
+import FloatLabel from 'primevue/floatlabel';
+import Button from 'primevue/button';
+
+const value = ref(null);
+</script>
