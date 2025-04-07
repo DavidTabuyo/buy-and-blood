eb3da18dc55daa65fc6de13c86d1fea9fd33f6d2
dario <dariommsr71@gmail.com>
2025-04-07 20:07:13 +0200

    FEAT: change value component

---
+++ frontend/src/components/utils/ChangeValue.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        {{ str }}
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps, watch, onMounted } from 'vue'
+
+const str = ref("");
+
+const props = defineProps({
+    value: Number
+});
+
+watch(props, () => {
+    // Signo
+    const sign = props.value >= 0 ? "+" : "-"
+
+    // Valor
+    const absValue = Math.abs(props.value);
+    const strValue = absValue.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    // Concatenar todo
+    str.value = sign + strValue
+}, { deep: true, immediate: true });
+</script>
