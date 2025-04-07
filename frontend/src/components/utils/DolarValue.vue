2eb4292399c1f05726fac3ddc9658f0759107a9f
dario <dariommsr71@gmail.com>
2025-04-07 20:06:56 +0200

    FEAT: dolar value component

---
+++ frontend/src/components/utils/DolarValue.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ str }}
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps, watch, onMounted } from 'vue'
+
+const str = ref("");
+
+const props = defineProps({
+    value: Number
+});
+
+watch(props, () => {
+    str.value = props.value.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}, { deep: true, immediate: true });
+</script>
