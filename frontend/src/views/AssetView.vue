e22940552764741e18e4816f5210d833142f7453
dario <dariommsr71@gmail.com>
2025-04-07 20:07:41 +0200

    FEAT: use the new componenets in asset page

--- frontend/src/views/AssetView.vue
+++ frontend/src/views/AssetView.vue
@@ -4,8 +4,10 @@
         <div class="w-2/3 flex flex-col h-full">
             <div>Fidelity S&P 500 Index Fund EUR P Acc</div>
             <div class="flex">
-                <div>234,23$</div>
-                <PercentageChange :value="changeValue" />
+                <div>
+                    <DolarValue :value="assetValue" />
+                </div>
+                <PercentageChange :value="percentageChange" />
             </div>
             <div class="flex-1">
                 <TradingViewChart class="h-full" />
@@ -13,26 +15,34 @@
         </div>
 
         <!-- Compra venta -->
-        <div class="w-1/3 flex flex-col items-center space-y-4 p-4 bg-white shadow-lg rounded-xl">
-            <div class="text-center">Mi Total:</div>
-            <div class="text-center">$12.234,34</div>
-            <div class="flex justify-center items-center gap-2">
-                <div>+$4233.23</div>
-                <PercentageChange :value="changeValue" />
-            </div>
-            <div class="flex justify-center gap-2">
-                <Button>Comprar</Button>
-                <Button>Vender</Button>
-            </div>
-            <FloatLabel variant="on" class="w-full flex flex-col items-center">
-                <div class="w-full flex justify-center">
-                    <InputNumber class="w-full text-center" v-model="value" inputId="on_label" mode="currency"
-                        currency="USD" locale="en-US" />
-                </div>
-                <label for="on-label" class="text-center">Introduce cantidad...</label>
-            </FloatLabel>
+        <div class="w-1/3 flex flex-col justify-between items-center space-y-4 p-4 bg-white shadow-lg rounded-xl">
             <div>
-                <Button>Confirmar</Button>
+                <div class="text-center">Mi Total:</div>
+                <div class="text-center">
+                    <DolarValue :value="myTotal" />
+                </div>
+                <div class="flex justify-center items-center gap-2">
+                    <ChangeValue :value="changeValue" />
+                    <PercentageChange :value="percentageChange" />
+                </div>
+            </div>
+            <div class="w-full flex flex-col items-center gap-4">
+                <div class="flex justify-center gap-2">
+                    <ButtonGroup class="flex">
+                        <Button label="Comprar" class="flex-1" :class="getButtonClass('opcion1')"
+                            @click="selectOption('opcion1')" />
+                        <Button label="Vender" class="flex-1" :class="getButtonClass('opcion2')"
+                            @click="selectOption('opcion2')" />
+                    </ButtonGroup>
+                </div>
+                <FloatLabel variant="on" class="w-full flex flex-col items-center">
+                    <div class="w-full flex justify-center">
+                        <InputNumber class="w-full text-center" v-model="value" inputId="on_label" mode="currency"
+                            currency="USD" locale="en-US" />
+                    </div>
+                    <label for="on-label" class="text-center">Introduce cantidad...</label>
+                </FloatLabel>
+                <Button class="w-full">Confirmar</Button>
             </div>
         </div>
 
@@ -46,7 +56,27 @@ import FloatLabel from 'primevue/floatlabel';
 import Button from 'primevue/button';
 import TradingViewChart from '@/components/asset/TradingViewChart.vue';
 import PercentageChange from '@/components/utils/PercentageChange.vue';
+import ChangeValue from '@/components/utils/ChangeValue.vue';
+import DolarValue from '@/components/utils/DolarValue.vue';
+import ButtonGroup from 'primevue/buttongroup';
 
 const value = ref(null);
-const changeValue = ref(-0.39876974);
+const assetValue = ref(260.34)
+const percentageChange = ref(-0.39876974);
+const changeValue = ref(0);
+const myTotal = ref(123342.45563);
+
+const selected = ref('opcion1'); // Opción por defecto seleccionada
+
+const selectOption = (option) => {
+    selected.value = option;
+};
+
+// Función para asignar clases dinámicamente
+const getButtonClass = (option) => {
+    return {
+        'p-button-primary': selected.value === option, // Color cuando está seleccionado
+        'p-button-outlined': selected.value !== option // Color por defecto
+    };
+};
 </script>
