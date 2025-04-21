9d036cbca0b80b6d64112bef8ff10456c990de96
davidTabuyo <dtabum00@estudiantes.unileon.es>
2025-04-21 18:30:01 +0200

    MISC: refractored urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 #views import
 from app.views.homepage import prueba_view
-from app.views.asset_page import asset_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/prueba/', prueba_view, name='prueba_api'),
-    path('api/asset/<str:ticker>/', asset_detail, name='asset-detail'),
+    path('api/asset/', include('app.urls.assets_urls')),    
+    path('api/invplan/', include('app.urls.invplan_urls')),    
 ]
