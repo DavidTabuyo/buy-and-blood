57dd5689371908d93a5c29e986de086f5f684274
davidTabuyo <dtabum00@estudiantes.unileon.es>
2025-04-30 17:03:22 +0200

    FEAT: finiched list endpoint

--- backend/app/views/asset_page.py
+++ backend/app/views/asset_page.py
@@ -28,8 +28,25 @@ def asset_detail(request, ticker):
 
 @api_view(['GET'])
 def asset_list(request):
-    #DEVOLVER TIKER EN VEZ DE ID
-    asset_ids = list(Asset.objects.values_list('id', flat=True))
+    # Captura los parámetros de la solicitud GET asignamos el valor None si no encontramos nada por parámetros
+    asset_type = request.GET.get('type', None)  
+    search = request.GET.get('search', None)
+
+    # Filtrar los activos según los parámetros recibidos
+    assets = Asset.objects.all()
+
+    if asset_type:
+        # Filtra por tipo si se pasó el parámetro 'type'
+        assets = assets.filter(type=asset_type)  
+    
+    if search:
+        # Filtra por nombre si se pasó el parámetro 'search'
+        assets = assets.filter(name__icontains=search)
+    
+    # Extraer solo los IDs de los activos filtrados
+    asset_ids = list(assets.values_list('id', flat=True))
+    
+    # Devolver los resultados
     return Response(asset_ids)
     
 @api_view(['GET'])
