ec20cae1c3df2a806a3bc6a1f0868e578ce5caf9
dario <dariommsr71@gmail.com>
2025-05-03 19:10:34 +0200

    FEAT: comucicacion con el backend hecha

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/prueba/', prueba_view, name='prueba_api'),
     path('api/asset/', include('app.api_urls.asset_urls')),    
-    path('api/invplan/', include('app.api_urls.invplan_urls')),    
+    path('api/invplan/', include('app.api_urls.invplan_urls')),
+    path('api/user/', include('app.api_urls.user_urls')),    
 ]
