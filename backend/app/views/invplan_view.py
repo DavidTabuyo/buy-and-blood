d18b650e7883c850b23153f79d857ce6eab5d671
Daniel Gregori <dgregg00@estudiantes.unileon.es>
2025-05-06 15:53:48 +0200

    FEAT: id plan's list done

--- backend/app/views/invplan_view.py
+++ backend/app/views/invplan_view.py
@@ -9,9 +9,17 @@ def invplan_details(request, id):
 
     #print("Dani dice: ", plan.name,"\nDani repite: ", plan.description)
 
+    #Los assets y el porcentaje se saca de la tabla plan_assets
+
     response_data = {
         'name': plan.name,
         'description': plan.description
     }
 
     return JsonResponse(response_data)
+
+@api_view(['GET'])
+def invplan_idlist(request):
+    ids = Plan.objects.values_list('id', flat=True)
+
+    return JsonResponse(list(ids))
