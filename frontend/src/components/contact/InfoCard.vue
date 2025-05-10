bd0e9be5b38bcffc90d6310ba7546100564b2eac
Daniel Gregori <dgregg00@estudiantes.unileon.es>
2025-05-10 14:19:11 +0200

    FIX: added image to infoCard and change text

--- frontend/src/components/contact/InfoCard.vue
+++ frontend/src/components/contact/InfoCard.vue
@@ -1,10 +1,15 @@
 <template>
-    <Card style="width: 100rem; overflow: hidden">
-        <template #title>Advanced Card</template>
+    <Card style="width: 30rem; height: auto; overflow: hidden; left: 10rem;" class="absolute">
+        <template #header>
+            <img alt="user header" src="/src/assets/future.jpg" />
+        </template>
+        <template #title>Sobre nosotros</template>
         <template #content>
-            <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-                quas!
+            <p class="m-0" style="text-align: justify;">
+                <br />Buy&Blood es una aplicación de inversión que permite comprar y vender activos financieros, gestionar carteras y acceder a planes de inversión personalizados.<br />
+                Su objetivo es simplificar el proceso de inversión, ofreciendo una plataforma accesible y eficiente.<br />
+                Dirigida a cualquier persona interesada en invertir, desde principiantes hasta inversores experimentados.<br />
+                Buy&Blood soluciona la complejidad de la inversión tradicional mediante una interfaz intuitiva y herramientas que permiten monitorear y optimizar las inversiones de manera sencilla.
             </p>
         </template>
         <template #footer>
