f9490cda7391f5ce119af8fab1841f3051391402
davidTabuyo <dtabum00@estudiantes.unileon.es>
2025-05-17 03:07:53 +0200

    FEAT: advanced w some visual bugs

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -103,7 +103,7 @@ const getInvestingPlan = () => {
 getInvestingPlan();
 
 const onRowClick = (event) => {
-    window.location.href = `/asset/${event.data.id}`;
+    window.location.href = `/asset/${event.data.asset_id}`;
 };
 
 </script>
