8519de7c04f5df437a32d690c1a7f4090c69ad4a
dario <dariommsr71@gmail.com>
2025-06-02 00:21:11 +0200

    FIX: developer cards

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -5,7 +5,7 @@
       <SecondCard class="w-1/3"></SecondCard>
       <ThirdCard class="w-1/3"></ThirdCard>
     </div>
-    <TeamCard class="mt-[10rem]"></TeamCard>
+    <TeamCard class="mt-[3rem]"></TeamCard>
   </div>
 </template>
   
@@ -13,5 +13,5 @@
   import ContactCard from '@/components/contact/InfoCard.vue';
   import SecondCard from '@/components/contact/SecondCard.vue';
   import ThirdCard from '@/components/contact/ThirdCard.vue';
-  import TeamCard from '../components/contact/TeamCard.vue';
+  import TeamCard from '@/components/contact/TeamCard.vue';
 </script>
